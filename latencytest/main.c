edef038b4380d554242b60a3527b2ee8f05e62a7
Dhiraj Gupta <dgupta45@in.ibm.com>
2020-01-26 17:16:55 +0530

    Initial Commit

---
+++ latencytest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  latencytest
+//
+//  Created by Dhiraj Gupta on 26/01/20.
+//  Copyright © 2020 Dhiraj Gupta. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
